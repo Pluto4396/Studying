fe9917d5828b5eb088fca60000e83568ff11d3f8
Pluto4396 <2043726287@qq.com>
2022-10-01 14:18:43 +0800

    Initial commit

---
+++ forHomework.java
@@ -0,0 +1,17 @@
+public class forHomework{
+	public static void main(String[]args){
+		int n = 0;
+		//Find multiples of nine
+		for (int i = 1;i <= 100 ;i++){
+			//Print these numbers and sum them to find the number
+			if(i % 9 == 0){
+				n++;
+				System.out.println(i);
+				int sum =+ i;
+				
+			}
+			
+		}
+		System.out.println(n);
+	}
+}
