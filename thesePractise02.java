e296e684f1ffa328f69dc1bec7df9a0dfdc33ea6
Pluto4396 <2043726287@qq.com>
2022-10-03 21:50:16 +0800

    fighting

---
+++ thesePractise02.java
@@ -0,0 +1,18 @@
+public class thesePractise02{
+	public static void main(String[]args){
+		int num1 = 1;
+		while(num1 <= 9){
+			int num3 = 1;
+		for(;num3 <=num1; num3++){
+			System.out.print(num1 + "*" + num3 + "=" + num1*num3 + "   ");	
+					if(num1 == num3){
+			System.out.print("\n");
+				}
+		
+			}
+			
+		
+		num1++;
+		}
+	}
+}
